03574e409f68a051f90fc89bd5bcf356598c2635
benutomo <52617232+benutomo-dev@users.noreply.github.com>
2025-03-17 18:41:13 +0900

    feat: add IExceptionFactory and support xunit

--- src/ShouldMethodAssertion/ShouldMethodDefinitions/AssertExceptionUtil.cs
+++ src/ShouldMethodAssertion/ShouldMethodDefinitions/AssertExceptionUtil.cs
@@ -2,13 +2,43 @@
 
 public static class AssertExceptionUtil
 {
-    public static Exception Create(string message)
+    private interface IExceptionFactory
     {
-        return new InvalidOperationException(message);
+        Exception Create(string message);
+        Exception Create(string message, Exception exception);
     }
 
-    public static Exception Create(string message, Exception exception)
+    private sealed class DefaultExceptionFactory : IExceptionFactory
     {
-        return new InvalidOperationException(message, exception);
+        public Exception Create(string message) => new ShouldMethodAssertion.ShouldMethodDefinitions.Exceptions.ShouldMethodAssertionException(message);
+
+        public Exception Create(string message, Exception exception) => new ShouldMethodAssertion.ShouldMethodDefinitions.Exceptions.ShouldMethodAssertionException(message, exception);
     }
+
+    private sealed class XunitExceptionFactory : IExceptionFactory
+    {
+        public Exception Create(string message) => new Xunit.Sdk.ShouldMethodAssertionException(message);
+
+        public Exception Create(string message, Exception exception) => new Xunit.Sdk.ShouldMethodAssertionException(message, exception);
+    }
+
+    private static Lazy<IExceptionFactory> _exceptionFactory = new Lazy<IExceptionFactory>(CreateFactory);
+
+    private static IExceptionFactory CreateFactory()
+    {
+        var x = AppDomain.CurrentDomain.GetAssemblies();
+
+        if (AppDomain.CurrentDomain.GetAssemblies().Any(v => v.GetName().Name is "xunit.core" or "xunit.v3.core"))
+        {
+            return new XunitExceptionFactory();
+        }
+        else
+        {
+            return new DefaultExceptionFactory();
+        }
+    }
+
+    public static Exception Create(string message) => _exceptionFactory.Value.Create(message);
+
+    public static Exception Create(string message, Exception exception) => _exceptionFactory.Value.Create(message, exception);
 }
---
+++ src/ShouldMethodAssertion/ShouldMethodDefinitions/Exceptions/ShouldMethodAssertionException.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Runtime.Serialization;
+
+namespace Xunit.Sdk
+{
+    internal sealed class ShouldMethodAssertionException : Exception
+    {
+        public ShouldMethodAssertionException()
+        {
+        }
+
+        public ShouldMethodAssertionException(string? message) : base(message)
+        {
+        }
+
+        public ShouldMethodAssertionException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+#pragma warning disable SYSLIB0051 // 型またはメンバーが旧型式です
+        public ShouldMethodAssertionException(SerializationInfo info, StreamingContext context) : base(info, context)
+#pragma warning restore SYSLIB0051 // 型またはメンバーが旧型式です
+        {
+        }
+    }
+}
+
+namespace ShouldMethodAssertion.ShouldMethodDefinitions.Exceptions
+{
+    internal sealed class ShouldMethodAssertionException : Exception
+    {
+        public ShouldMethodAssertionException()
+        {
+        }
+
+        public ShouldMethodAssertionException(string? message) : base(message)
+        {
+        }
+
+        public ShouldMethodAssertionException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+
+#pragma warning disable SYSLIB0051 // 型またはメンバーが旧型式です
+        public ShouldMethodAssertionException(SerializationInfo info, StreamingContext context) : base(info, context)
+#pragma warning restore SYSLIB0051 // 型またはメンバーが旧型式です
+        {
+        }
+    }
+}
