97dd6485a5f4067d8a33ce4a6f764f2e8ded828a
benutomo <52617232+benutomo-dev@users.noreply.github.com>
2025-03-17 23:20:17 +0900

    test: GuidShouldBeEmptyTests

---
+++ test/ShouldMethodAssertion.Tests/ShouldMethodDefinitions/GuidShouldBeEmptyTests.cs
@@ -0,0 +1,32 @@
+﻿using ShouldMethodAssertion.ShouldExtensions;
+
+namespace ShouldMethodAssertion.Tests.ShouldMethodDefinitions;
+
+public class GuidShouldBeEmptyTests
+{
+    [Fact]
+    public void ShouldBeEmpty_NotFail()
+    {
+        default(Guid).Should().BeEmpty();
+        Guid.Empty.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ShouldBeEmpty_Fail()
+    {
+        Assert.Throws<Xunit.Sdk.ShouldMethodAssertionException>(() => Guid.NewGuid().Should().BeEmpty());
+    }
+
+    [Fact]
+    public void ShouldNotBeEmpty_NotFail()
+    {
+        Guid.NewGuid().Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public void ShouldNotBeEmpty_Fail()
+    {
+        Assert.Throws<Xunit.Sdk.ShouldMethodAssertionException>(() => default(Guid).Should().NotBeEmpty());
+        Assert.Throws<Xunit.Sdk.ShouldMethodAssertionException>(() => Guid.Empty.Should().NotBeEmpty());
+    }
+}
